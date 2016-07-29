181e427274c9f66be6ddb59007a398fc1d43b851
Nathan Reed <reednj@gmail.com>
2016-07-29 08:47:18 +0800

    update deply script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+CODE=/home/reednj/code
+APP=$CODE/redditcounter.git
+CONFIG=$CODE/config_backup/redditcounter/
 
+# just copy everything from the config backup into
+# the app folder...
+cp $CONFIG/* $APP/
