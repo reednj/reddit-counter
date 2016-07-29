91fb171c89bf2e350cf4f8ca0850655c0f1a1d60
Nathan Reed <reednj@gmail.com>
2016-07-29 08:05:50 +0800

    initial comment

---
+++ app.rb
@@ -0,0 +1,45 @@
+require 'rest_client'
+require 'yaml'
+
+require './model'
+
+class App
+	def main
+		update_counter
+	end
+
+	def update_counter
+		comment_count = get_latest_id
+		current_count = TagValue.for_tag('reddit-comment-count', comment_count).save
+
+		comment_rate = comments_per_second(current_count, current_count.prev)
+		current_rate = TagValue.for_tag('reddit-comment-rate', comment_rate).save
+	end
+
+	def comments_per_second(current, prev)
+		return nil if current.nil? || prev.nil?
+
+		delta_v = current.value - prev.value
+		delta_t = current.created_date - prev.created_date
+		delta_v.to_f / delta_t.to_f 
+	end
+
+	def get_latest_id
+		get_latest.first[:id].to_i 36
+	end
+
+	def get_latest
+		user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)'
+		text_data = RestClient.get "http://www.reddit.com/comments.json?limit=2&sort=new", :user_agent => user_agent
+		data = JSON.parse text_data, :symbolize_names => true
+		return data[:data][:children].map { |c| c[:data]  }
+	end
+end
+
+class Time
+	def age
+		Time.now - self
+	end
+end
+
+App.new.main
---
+++ model.rb
@@ -0,0 +1,37 @@
+require 'sequel'
+
+DB = Sequel.connect({
+	:adapter => 'mysql2',
+	:host => '127.0.0.1',
+	:username => 'linkuser',
+	:password => '',
+	:database => 'scratch'
+})
+
+class TagValue < Sequel::Model(:tag_values)
+	def self.for_tag(tag_id, value)
+		self.new do |v|
+			v.tag_id = tag_id
+			v.value = value
+		end
+	end
+	
+	def value
+		tag_value || tag_value_bigint
+	end
+
+	def value=(v)
+		if v.is_a?(Fixnum) && v > 2**31
+			self.tag_value_bigint = v
+		else
+			self.tag_value = v
+		end
+	end
+
+	def prev
+		@prev ||= self.class.
+			where('tag_id = ? and value_id < ?', self.tag_id, self.value_id).
+			reverse_order(:value_id).
+			first
+	end
+end
