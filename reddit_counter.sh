103f652fb1a8c1736c7b80730c8ecd816e3b997b
Nathan <reednj@gmail.com>
2017-10-25 09:47:00 +0800

    add rbenv to the data update script

--- reddit_counter.sh
+++ reddit_counter.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+export RBENV_ROOT="${HOME}/.rbenv"
+
+if [ -d "${RBENV_ROOT}" ]; then
+  export PATH="${RBENV_ROOT}/bin:${PATH}"
+  eval "$(rbenv init -)"
+fi
+
 cd `dirname "$0"`
 bundle exec ruby update-data.rb
