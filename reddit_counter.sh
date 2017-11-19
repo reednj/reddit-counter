00f4d4d3d9a9a90ff969fd235e35a82d8bc4847d
Nathan <reednj@gmail.com>
2017-11-19 08:57:52 +0800

    new script to make running ruby scripts easier
    so they load rbenv properly etc

--- reddit_counter.sh
+++ reddit_counter.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-export RBENV_ROOT="${HOME}/.rbenv"
-
-if [ -d "${RBENV_ROOT}" ]; then
-  export PATH="${RBENV_ROOT}/bin:${PATH}"
-  eval "$(rbenv init -)"
-fi
-
-cd `dirname "$0"`
-bundle exec ruby update_data.rb
+./_be.sh update_data.rb
