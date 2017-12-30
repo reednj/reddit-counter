b3ca5a8228bc8577e8365b72e2f194edeee354d1
Nathan <reednj@gmail.com>
2017-12-30 10:39:17 +0800

    new rbenv script to make sure proper ruby version runs even over ssh

---
+++ _rbenv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# loads rbenv and runs the script with
+# bundle execs
+
+export RBENV_ROOT="${HOME}/.rbenv"
+
+if [ -d "${RBENV_ROOT}" ]; then
+  export PATH="${RBENV_ROOT}/bin:${PATH}"
+  eval "$(rbenv init -)"
+fi
