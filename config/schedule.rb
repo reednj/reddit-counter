5be8fd9554cba0b57d59598d7d9611f28b4dea60
Nathan <reednj@gmail.com>
2018-01-04 06:31:34 +0800

    reduce data updates

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
 job_type :sh, 'cd :path && :task :output'
 
-every 1.minute do
+every 2.minute do
     sh "./_be.sh update_top_threads.rb"
 end
 
-every 5.minutes do
+every 15.minutes do
     sh "./_be.sh update_data.rb"
 end
